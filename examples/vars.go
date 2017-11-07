1e0c4d634762fa2d751eb32f1ec1e6b8d89f967a
Miki Tebeka <miki.tebeka@gmail.com>
2017-11-07 07:25:33 +0200

    vars

---
+++ examples/vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	fmt.Println(i)
+
+	var j int = 7
+	fmt.Println(j)
+
+	k := 8
+	fmt.Println(k)
+
+	x, y := 1, "hello"
+	fmt.Println(x, y)
+}
